8f66f6f0ec3782842d36c387ad9d5a0947797d4c
Nahyeon Lee <naboee0627@gmail.com>
2020-02-18 22:03:39 +0900

    mysql 패키지 테스트

--- api/db/mysql.js
+++ api/db/mysql.js
@@ -1,3 +1,13 @@
+const mysql = require('mysql')
 const connectionInfo = require('../../credentials/mysql')
 
-console.log(connectionInfo);
+var connection = mysql.createConnection(connectionInfo)
+
+connection.connect()
+
+connection.query('SELECT * FROM item', (err, res) => {
+    if (err) throw err
+    console.log(res[0])
+})
+
+connection.end()
