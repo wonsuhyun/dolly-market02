073442c270bd814233711ad94165db9140d7a999
Nahyeon Lee <naboee0627@gmail.com>
2020-02-19 23:18:13 +0900

    죽이되든 밥이되든 item 리스트 불러옴

--- api/db/mysql.js
+++ api/db/mysql.js
@@ -1,15 +1,6 @@
-const mysql = require('mysql')
-const connectionInfo = require('../../credentials/mysql')
+import mysql from 'mysql2/promise'
+import connectionInfo from'../../credentials/mysql'
 
-var connection = mysql.createConnection(connectionInfo)
+const pool = mysql.createPool(connectionInfo);
 
-connection.connect()
-
-const items = connection.query('SELECT * FROM item', (err, res) => {
-    if (err) throw err
-    return res[0]
-})
-
-connection.end()
-
-export { items }
+export default pool
--- api/routes/item.js
+++ api/routes/item.js
@@ -1,11 +1,20 @@
-const router = require('express').Router()
+import express from 'express'
 import Item from '../model/Item'
-const mysql = require('../db/mysql')
+import pool from '../db/mysql'
 
-router.get('/', (req, res, next) => {
-    // const items = new Item({...mysql[0]})
-    console.log(items)
-    res.send('hello ' + Math.random())
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    const connection = await pool.getConnection(async conn=>conn);
+    await connection.beginTransaction();
+
+    const [rows] = await connection.query('SELECT * FROM item')
+
+    await connection.commit();
+    connection.release();
+
+    console.log(rows)
+    res.send(rows)
 })
 
 module.exports = router
