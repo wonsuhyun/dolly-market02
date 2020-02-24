b798028ecc929dd3a22cb40b0285e546b7fd00de
wonsuhyun <sh.won@quintet.co.kr>
2020-02-24 17:18:04 +0900

    main

--- store/index.js
+++ store/index.js
@@ -15,8 +15,23 @@ export const mutations = {
     }
 }
 
-export const action = {
-    testAction({commit},item){
-        commit("testmutation",item)
+export const actions = {
+    testAction({commit},items){
+        items.map( item =>{
+            var today = new Date;
+            var week = new Date(today.getFullYear(),today.getMonth(),today.getDate()-7);
+            var itemArr = item.create_date.split('-');
+            var createDate = new Date(itemArr[0],itemArr[1]-1, Number(itemArr[2].slice(0,2)))
+            itemArr[2] = itemArr[2].slice(0,2)
+            var dateSet = itemArr.join('.')
+            item.create = dateSet
+
+            if( week < createDate ){
+                item.isNew=true;
+            }else{
+                item.isNew=false;
+            }    
+        })
+        commit("setItem",items)
     }
 }
