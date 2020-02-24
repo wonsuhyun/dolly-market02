e50ff181554b9d9526d057391b47357bccf30597
Nahyeon Lee <naboee0627@gmail.com>
2020-02-24 12:49:24 +0900

    서버 시작점 index.js 임시 방편으로 갖다붙여놓음

--- api/runner.js
+++ api/runner.js
@@ -1,3 +1,3 @@
 // Set options as a parameter, environment variable, or rc file.
 require = require("esm")(module/*, options*/)
-module.exports = require("./index.js")
+module.exports = require("./server-index.js")
