537d7810d9c9f17d30524ee4d8870d47157a9a3d
Nahyeon Lee <naboee0627@gmail.com>
2020-02-26 18:22:03 +0900

    라우터명 변경

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { itemRouter } from './router'
+import createError from 'http-errors'
+import { itemRoute } from './route'
 
 // server settings
 const app = express()
@@ -8,8 +9,8 @@ const isAPIDev = (process.argv[1].includes('api-runner.js')) ? true : false
 // middlewares
 app.use(express.json())
 
-// routers
-app.use(['/items', '/api/items'], itemRouter)
+// routes
+app.use(['/items', '/api/items'], itemRoute)
 
 if (isAPIDev) {
   const port = process.env.PORT || 3000
@@ -18,6 +19,22 @@ if (isAPIDev) {
     console.log('Dolly server is up on', port)
   })
 }
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err.status, message: res.locals.message });
+});
 
 module.exports = {
   path: '/api',
