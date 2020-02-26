e66b4d6d9d99b5965078c409c0ef931be4c2521d
nh0627 <naboee0627@gmail.com>
2020-02-26 01:50:40 +0000

    환경 분리

--- api/index.js
+++ api/index.js
@@ -1,10 +1,52 @@
+// import libraries
 import express from 'express'
+import createError from 'http-errors'
+
+// server settings
 const app = express()
-const ROUTES_PATH = './router'
+const port = process.env.PORT || 3000
+app.use(express.json())
+const isAPIDev = (process.argv[1].includes('api-runner.js')) ? true : false
 
-app.use('/item', require(`${ROUTES_PATH}/item`))
+/*
+  Routers(Endpoint, controllers)
+*/
+// define router folders
+const itemRouter = require('./router/item')
 
-module.exports = {
-  path: '/api',
-  handler: app
+// set routes
+app.use(['/item', '/api/item'], itemRouter)
+// add router here ! ! ! ★
+
+/*
+  Error Handling
+*/
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.json({ error: err.status, message: res.locals.message })
+});
+
+if (isAPIDev) {
+  // Server listener
+  app.listen(port, () => {
+    console.log('Dolly server is up on', port)
+  })
+  
+  module.exports = app
+} else {
+  module.exports = {
+    path: '/api',
+    handler: app
+  }
 }
