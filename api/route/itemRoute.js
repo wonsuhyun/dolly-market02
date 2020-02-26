48d9f9fffbe3700c9f15cf37d39e413be4d92bfd
Nahyeon Lee <naboee0627@gmail.com>
2020-02-26 23:22:13 +0900

    route 공통화 처리 필요함

--- api/route/itemRoute.js
+++ api/route/itemRoute.js
@@ -6,20 +6,20 @@ const router = express.Router()
 /* 
 Get item list
 */
-router.get('/', wrapAsync(async (req, res) => {
+router.get('/', errorCaptor(async (req, res) => {
     res.json(await itemService.get());
 }))
 
 /* 
 Get item detail
 */
-router.get(["/:pid"], wrapAsync(async (req, res) => {
+router.get(["/:pid"], errorCaptor(async (req, res) => {
     const pid = req.params.pid.toUpperCase()
     const item = await itemService.getById(pid)
     res.json(item)
 }))
 
-function wrapAsync(fn) {
+function errorCaptor(fn) {
     return (req, res, next) => fn(req, res, next).catch(next)
 }
 
