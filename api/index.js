e9d60231d124be9a550c07ac38b1147fe9f570ed
Nahyeon Lee <naboee0627@gmail.com>
2020-03-26 22:41:24 +0900

    조금 수정

--- api/index.js
+++ api/index.js
@@ -1,13 +1,47 @@
-const ExpressAPI = require('./expressAPI')
+import passport from 'passport'
+import { passportStrategy } from './util'
+import createError from 'http-errors'
+import { itemRoute, authRoute } from './route'
+import ExpressBase from '../server/expressBase'
+
 const isAPI = process.env.NODE_ENV == 'api'
-const expressAPI = new ExpressAPI()
 
-const runExpress = expressAPI.run()
+class ExpressAPI extends ExpressBase {
 
-const apiModule = {
-    path: '/api',
-    handler: expressAPI.express
+    constructor() {
+        super()
+        this.run()
+    }
+
+    run() {
+        // middlewares
+        this.express.use(this.json)
+        this.express.use(passport.initialize())
+        passportStrategy()
+
+        // routes
+        this.express.use(['/items', '/api/items'], itemRoute)
+        this.express.use(['/auth', '/api/auth'], authRoute)
+
+        if (isAPI) super.run()
+
+        this.errorHandler()
+    }
+
+    errorHandler() {
+        this.express.use((req, res, next) => {
+            next(createError(404))
+        })
+
+        this.express.use((error, req, res, next) => {
+            const { status, message } = error
+            res.status(status || 500)
+            res.json({ status, message })
+        })
+    }
 }
 
-module.exports = (isAPI) ? runExpress : apiModule
-
+module.exports = {
+    path: '/api',
+    handler: new ExpressAPI().express
+}
