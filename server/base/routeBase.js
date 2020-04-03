ca70a7392d838e8e5517db47b2369ebcd017897a
Nahyeon Lee <naboee0627@gmail.com>
2020-04-03 17:50:22 +0900

    조금 수정

--- server/base/routeBase.js
+++ server/base/routeBase.js
@@ -17,7 +17,7 @@ class RouteBase {
         this.router[httpMethod](`${this.path + uri}`, this.asyncWrapper(boundAction))
     }
 
-    asyncWrapper = (boundAction) => {
+    asyncWrapper(boundAction) {
         return (req, res, next) => boundAction.bind(this.controller)(req, res, next).catch((err) => {
             errorToNext(err, next)
         })
